44cfcd9571f392852f6453aa6715659924555cb8
omarsemsovic <omar2008semsovic@gmail.com>
2022-11-03 20:59:56 +0100

    ...

---
+++ java_script/8 cas/app.js
@@ -0,0 +1,11 @@
+let rencencia = "danas cemo raditi neke operacije sa stringovima";
+console.log(rencencia);
+let omar = "omar"
+
+console.log([omar]);
+console.log([omar]);
+
+let duzinaRecenice = rencencia.length
+c(duzinaRecenice)
+
+
